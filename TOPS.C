0886de52dc0d99632e9bf2ed9353bf054e4907b5
Aayushigoria15 <157952434+Aayushigoria@users.noreply.github.com>
2025-07-29 14:58:26 +0530

    Daily-task

---
+++ TOPS.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	clrscr();
+	printf("\nEnter n:");
+	scanf("%d",&n);
+	while(n>0)
+	{
+	       printf("\nTops Technology");
+	       n--;
+	}
+	getch();
+}
+
+
+
+
+
